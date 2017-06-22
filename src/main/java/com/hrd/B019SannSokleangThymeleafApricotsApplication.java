261790f9b0bef97a93614af59dd281803b207e26
DESKTOP-CR3UEHT\Leang StoneBank <sokleangsann@gmail.com>
2017-06-23 01:20:20 +0700

    fisnish mini project

---
+++ src/main/java/com/hrd/B019SannSokleangThymeleafApricotsApplication.java
@@ -0,0 +1,12 @@
+package com.hrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B019SannSokleangThymeleafApricotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B019SannSokleangThymeleafApricotsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hrd/B019SannSokleangThymeleafApricotsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hrd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class B019SannSokleangThymeleafApricotsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
